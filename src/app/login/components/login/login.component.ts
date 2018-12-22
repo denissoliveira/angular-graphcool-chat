d7ecc0e1d2af3f9a1e145ea48de2961bd59aed98
denissoliveira <denissoliveira@gmail.com>
2018-12-22 16:36:11 -0300

    feat: LoginComponnt: exibindo mensagns de validação

--- src/app/login/components/login/login.component.ts
+++ src/app/login/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.createForm();
   }
+
+  createForm(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(5)]]
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.loginForm.value);
+  }
+
+  // cria um atributo, não um método. ao chama-lo ele atribui o conteúdo, email vira um tipo FormControl
+  // com o conteúdo do return
+  get email(): FormControl { return <FormControl>this.loginForm.get('email'); }
+  get password(): FormControl { return <FormControl>this.loginForm.get('password'); }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule, MatToolbarModule, MatFormFieldModule, MatInputModule, MatButtonModule } from '@angular/material';
 
 @NgModule({
   exports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatToolbarModule,
+    ReactiveFormsModule
   ]
 })
 export class SharedModule { }

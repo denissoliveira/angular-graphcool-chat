abacf1d17e17de48ea6b657c8cca3bb9c04e8938
denissoliveira <denissoliveira@gmail.com>
2018-12-21 21:44:38 -0300

    feat: Criando LoginComponent com Angular CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { LoginComponent } from './login/components/login/login.component';
 import { LoginModule } from './login/login.module';
 
 @NgModule({
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent],
+  exports: [LoginComponent],
   imports: [
     SharedModule, // modulo que compartilha recursos por toda a app
     LoginRoutingModule

cf4ad5567070278fda9a5dea613bf702b32304ab
denissoliveira <denissoliveira@gmail.com>
2018-12-29 17:51:13 -0300

    feat: Carregando DashboardModule sob demanda com Lazy Loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './login/auth.guard';
 
 const routes: Routes = [
+  { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule', canLoad: [ AuthGuard ]},
   // pathMatch: 'full' retorna true se o caminho realmente for igual ao path
-  { path: '', redirectTo: 'login', pathMatch: 'full'}
+  { path: '', redirectTo: 'login', pathMatch: 'full'},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
